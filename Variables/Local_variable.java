1a5236aa9ede7bec4a6d181e8265843bf342a041
sahil-shahare <sahilshahare380@gmail.com>
2025-08-11 17:03:06 +0530

    Local_variable

--- Variables/Local_variable.java
+++ Variables/Local_variable.java
@@ -1,12 +1,14 @@
+package Variables;
+
 public class Local_variable {
     public static void main(String[] args) {
-        Animal a =new Animal();
+        Animal a = new Animal();
         a.run();
     }
 }
 
-class Animal{
-    public void run(){
+class Animal {
+    public void run() {
         System.out.println("running");
     }
 }
