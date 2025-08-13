324bf91c560ce2c5e7168309b2e3d6e846b952fe
sahil-shahare <sahilshahare380@gmail.com>
2025-08-13 17:29:46 +0530

    Final

---
+++ Variables/Final.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Final {
+
+    public static void main(String[] args) {
+        final int age = 100; // Do not change or reassign value
+        // age = 101;
+        System.out.println(age);
+    }
+}
