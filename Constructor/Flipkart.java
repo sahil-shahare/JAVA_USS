9277c72994b37ec6befce124806cfa6a4de89d92
sahil-shahare <sahilshahare380@gmail.com>
2025-09-23 23:12:31 +0530

    N

--- Constructor/Flipkart.java
+++ Constructor/Flipkart.java
@@ -3,15 +3,28 @@ package Constructor;
 public class Flipkart {
     public static void main(String[] args) {
 
-        // Create a object
-        Order c1 = new Order("Sahil", "948484848", "Pune", "441904");
+        // Using different constructors
+        Order c1 = new Order("Sahil", "948484848", "Pune", "441904");  // full constructor
+        Order c2 = new Order("Riya", "9876543210");                    // only name & mobile
+        Order c3 = new Order("Amit");                                  // only name
+        Order c4 = new Order();                                        // default constructor
+
+        // Deliver orders
         c1.deliver();
-        System.out.println(c1.name + " | " + c1.mobile + " | " + c1.city + " | " + c1.pincode + " | " + c1.price);
+        c2.deliver();
+        c3.deliver();
+        c4.deliver();
+
+        // Print details
+        c1.showDetails();
+        c2.showDetails();
+        c3.showDetails();
+        c4.showDetails();
     }
 }
 
 class Order {
-    // Declare Instance variable
+    // Instance variables
     String name;
     String mobile;
     String city;
@@ -20,16 +33,44 @@ class Order {
     String aadhar;
     String pan;
 
-    // Create a parameterized Constructor
-    public Order(String cus_name, String cus_mobile, String cus_city, String cus_pincode) {
-        this.name = cus_name;
-        this.mobile = cus_mobile;
-        this.city = cus_city;
-        this.pincode = cus_pincode;
+    // Default Constructor
+    public Order() {
+        this.name = "Unknown";
+        this.mobile = "N/A";
+        this.city = "N/A";
+        this.pincode = "000000";
     }
 
+    // Constructor with only name
+    public Order(String name) {
+        this.name = name;
+        this.mobile = "N/A";
+        this.city = "N/A";
+        this.pincode = "000000";
+    }
+
+    // Constructor with name & mobile
+    public Order(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+        this.city = "N/A";
+        this.pincode = "000000";
+    }
+
+    // Full Constructor
+    public Order(String name, String mobile, String city, String pincode) {
+        this.name = name;
+        this.mobile = mobile;
+        this.city = city;
+        this.pincode = pincode;
+    }
+
+    // Method
     public void deliver() {
-        System.out.println("Order delivered to : " + this.name);
+        System.out.println("Order delivered to : " + this.name + " (" + this.city + ")");
     }
 
+    public void showDetails() {
+        System.out.println(this.name + " | " + this.mobile + " | " + this.city + " | " + this.pincode);
+    }
 }
--- If_else/If_else.java
+++ If_else/If_else.java
@@ -1,19 +1,15 @@
- public class If_else{
+public class If_else {
     public static void main(String[] args) {
         double percentage = -3;
-        if(percentage>=35&&percentage<=50){
+        if (percentage >= 35 && percentage <= 50) {
             System.out.println("Pass");
-        }
-        else if(percentage>=51&&percentage<=60){
+        } else if (percentage >= 51 && percentage <= 60) {
             System.out.println("Second");
-        }
-        else if (percentage>=61&&percentage<=70) {
+        } else if (percentage >= 61 && percentage <= 70) {
             System.out.println("First");
-        }
-        else if(percentage>=71&&percentage<=100){
+        } else if (percentage >= 71 && percentage <= 100) {
             System.out.println("Dist");
-        }
-        else{
+        } else {
             System.out.println("Hard Luck");
         }
     }
