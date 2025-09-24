ccce9404985275a5de867e28452f3cddec79d7f3
sahil-shahare <sahilshahare380@gmail.com>
2025-09-24 10:26:29 +0530

    Inheritance

---
+++ OOPs/Inheritance.java
@@ -0,0 +1,42 @@
+package OOPs;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.show();
+    }
+}
+
+class Parent {
+    int num = 100;
+
+    // Non-Parameterized Constructor
+    public Parent() {
+        System.out.println("Parent Constructor");
+    }
+
+    // Parameterized Constructor
+    public Parent(String msg) {
+        System.out.println("Parent Constructor with message: " + msg);
+    }
+
+    // Method
+    public void display() {
+        System.out.println("Parent Display");
+    }
+}
+
+class Child extends Parent {
+    int num = 200;
+
+    public Child() {
+        super("Hello from Child");// Calls parameterized constructor of Parent
+        System.out.println("Child Constructor");
+    }
+
+    public void show() {
+        System.out.println("Child num = " + num);
+        System.out.println("Parent num = " + super.num);// Access parent variable
+        super.display();// call parent method
+    }
+}
