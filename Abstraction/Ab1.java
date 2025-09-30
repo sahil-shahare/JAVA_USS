c39286705a17dac48196d0e3743a01125c71920c
sahil-shahare <sahilshahare380@gmail.com>
2025-09-30 13:39:24 +0530

    Abstraction

---
+++ Abstraction/Ab1.java
@@ -0,0 +1,21 @@
+abstract class Animal {
+    abstract void sound(); // abstract method
+
+    void eat() { // concrete method
+        System.out.println("Animals can eat");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Ab1 {
+    public static void main(String[] args) {
+        Animal obj = new Dog();
+        obj.sound(); // Dog barks
+        obj.eat();   // Animals can eat
+    }
+}
