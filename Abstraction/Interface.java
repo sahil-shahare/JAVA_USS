cc9502cbd4a6b9e2263c402709c7cf87190835c5
sahil-shahare <sahilshahare380@gmail.com>
2025-10-01 09:52:13 +0530

    Interface

---
+++ Abstraction/Interface.java
@@ -0,0 +1,23 @@
+public class Interface {
+    public static void main(String[] args) {
+        I_UPI ip = new Phone_pe();
+
+        System.out.println(ip.sendmoney_to_account("123456", 40002.53));
+    }
+}
+
+interface I_UPI{
+    public String sendmoney_to_account(String account_number, double amount);
+
+    public String recievemoney(String number, double amount);
+}
+
+class Phone_pe implements I_UPI{
+    public String sendmoney_to_account(String account_number, double amount){
+        return "Money send to: "+account_number+" Amount : "+amount;
+    }
+
+    public String recievemoney(String number, double amount){
+        return null;
+    }
+}
