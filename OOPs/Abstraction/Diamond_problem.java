385e81931fea3d7f21e5a70b9a53e2fb11bef71d
sahil-shahare <sahilshahare380@gmail.com>
2025-10-04 14:08:06 +0530

    Diamond_Problem

--- OOPs/Abstraction/Diamond_problem.java
+++ OOPs/Abstraction/Diamond_problem.java
@@ -1,25 +1,26 @@
 package Abstraction;
+
 public class Diamond_problem {
     public static void main(String[] args) {
-        C c=new C();
+        C c = new C();
         c.show();
     }
 }
 
 interface A {
-    default void show(){
+    default void show() {
         System.out.println("A");
     }
 }
 
 interface B {
-    default void show(){
+    default void show() {
         System.out.println("B");
     }
 }
 
-class C implements A,B{
-    public void show(){
+class C implements A, B {
+    public void show() {
         System.out.println("C");
         A.super.show();
         B.super.show();
