f45bfc96f47e57fed42ca23146ec24040c81d307
sahil-shahare <sahilshahare380@gmail.com>
2025-10-07 10:56:22 +0530

    Functional_Interface

---
+++ OOPs/Abstraction/Functional_interface.java
@@ -0,0 +1,33 @@
+package Abstraction;
+
+public class Functional_interface {
+
+    public static void main(String[] args) {
+        // 1st way
+        // Operation op = new Operation();
+        // System.out.println(op.add(10, 10));
+
+        // 2nd way Anonymous Class
+        // Calculator c = new Calculator() {
+        // public int add(int a, int b) {
+        // return a + b;
+        // }
+        // };
+        // System.out.println(c.add(10, 20));
+
+        // 3rd way Lambda Expression
+        Calculator c = (a, b) -> a + b;
+        System.out.println(c.add(5, 10));
+    }
+}
+
+interface Calculator {
+    public int add(int a, int b);
+}
+
+// 1st way
+// class Operation implements Calculator {
+// public int add(int a, int b) {
+// return a + b;
+// }
+// }
