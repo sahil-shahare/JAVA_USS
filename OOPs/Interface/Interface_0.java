5adf084926d601ef9e16649e47e34df532c52fa3
sahil-shahare <sahilshahare380@gmail.com>
2025-10-09 16:29:13 +0530

    Interface

--- OOPs/Interface/Interface_0.java
+++ OOPs/Interface/Interface_0.java
@@ -1,4 +1,5 @@
 package Interface;
+
 public class Interface_0 {
     public static void main(String[] args) {
         I_UPI ip = new Phone_pe();
@@ -8,17 +9,22 @@ public class Interface_0 {
 }
 
 interface I_UPI {
+    double total=0;
     public String sendmoney_to_account(String account_number, double amount);
 
     public String recievemoney(String number, double amount);
 }
 
 class Phone_pe implements I_UPI {
+    double total = 0;
+    @Override
     public String sendmoney_to_account(String account_number, double amount) {
         return "Money send to: " + account_number + " Amount : " + amount;
     }
 
+    @Override
     public String recievemoney(String number, double amount) {
+        this.total = amount;
         return null;
     }
 }
