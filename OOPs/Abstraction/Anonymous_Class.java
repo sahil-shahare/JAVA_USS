ea8d1d66c74d974725d086b5b4abc807afa0ac79
sahil-shahare <sahilshahare380@gmail.com>
2025-10-09 14:20:29 +0530

    Anonymous_Class

--- OOPs/Abstraction/Anonymous_Class.java
+++ OOPs/Abstraction/Anonymous_Class.java
@@ -1,16 +1,16 @@
 package Abstraction;
 
 public class Anonymous_Class {
-   public static void main(String[] args) {
-    City city= new City() {
-        public void showpin(String pin){
-            System.out.println(pin);
-        }
-    };
-    city.showpin("441026");
-   } 
+    public static void main(String[] args) {
+        City city = new City() {
+            public void showpin(String pin) {
+                System.out.println(pin);
+            }
+        };
+        city.showpin("441026");
+    }
 }
 
 abstract class City {
-    abstract public void showpin(String pin);   
+    abstract public void showpin(String pin);
 }
