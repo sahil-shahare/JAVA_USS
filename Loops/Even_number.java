92d5828024b9a8c333bc60f86041c1e117b3fc00
sahil-shahare <sahilshahare380@gmail.com>
2025-10-12 11:36:21 +0530

    Even_number

--- Loops/Even_number.java
+++ Loops/Even_number.java
@@ -1,14 +1,12 @@
-public class Even_number{
-    public static void main (String[] args){
-        int num=20;
-        for(int i=0; i<=num; i++){
-            if(i%2==0){
+public class Even_number {
+    public static void main(String[] args) {
+        int num = 20;
+        for (int i = 0; i <= num; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i + " is an even number");
-            }
-            else {
+            } else {
                 System.out.println(i + " is an odd number");
             }
         }
     }
-
 }
