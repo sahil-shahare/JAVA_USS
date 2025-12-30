dee894c2b22c48b2c763bd08454aa0be573dedb3
sahil-shahare <sahilshahare380@gmail.com>
2025-12-31 00:00:02 +0530

    JCF-Vector

---
+++ Collection_Framework/List/VectorExample.java
@@ -0,0 +1,11 @@
+package Collection_Framework.List;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        List<String> vec = new Vector<String>();
+        
+    }
+}
