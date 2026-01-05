b6806d8eb9ac7859f1c5a8c6f6d9342dc927e518
sahil-shahare <sahilshahare380@gmail.com>
2026-01-06 00:04:34 +0530

    JCF-Set-LinkedHashSet

--- Collection_Framework/Set/LinkedHashSet.java
+++
@@ -1,5 +0,0 @@
-package Collection_Framework.Set;
-
-public class LinkedHashSet {
-    
-}
---
+++ Collection_Framework/Set/LinkedHashSetExample.java
@@ -0,0 +1,11 @@
+package Collection_Framework.Set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        Set<Integer> set = new LinkedHashSet<>();
+        set.add(2);
+    }
+}
