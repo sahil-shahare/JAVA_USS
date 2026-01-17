9e3f1cd0e2deb184bdb4bbab3a52a40b464f5115
sahil-shahare <sahilshahare380@gmail.com>
2026-01-18 00:12:27 +0530

    JCF-Iterator-Example

--- Collection_Framework/IteratorExample.java
+++ Collection_Framework/IteratorExample.java
@@ -8,7 +8,7 @@ public class IteratorExample {
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             list.add(i);
         }
 
