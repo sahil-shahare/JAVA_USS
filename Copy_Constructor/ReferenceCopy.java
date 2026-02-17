220e5c929cafb1b77f820a96780bca565f18d3e7
sahil-shahare <sahilshahare380@gmail.com>
2026-02-17 10:47:50 +0530

    Copy_Constructor

--- Copy_Constructor/ReferenceCopy.java
+++ Copy_Constructor/ReferenceCopy.java
@@ -2,19 +2,26 @@ package Copy_Constructor;
 
 public class ReferenceCopy {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
         Student s1 = new Student(1, "Carl");
-        Student s2 = new Student(s1);
+        Student s2 = s1; // RF
+        Student s3 = new Student(s1); // CC
 
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
+        Student s4 = (Student) s1.clone();// CC W Cloneable
+
+        // Shallow Copy -> Has A Relation with Single Object
+
+        System.out.println(s4);
+
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // System.out.println(s1.hashCode());
+        // System.out.println(s2.hashCode());
     }
 }
 
-class Student {
+class Student implements Cloneable {
     int id;
     String name;
 
@@ -23,8 +30,18 @@ class Student {
         this.name = name;
     }
 
-    public Student(Student s) {
+    public Student(Student s) { // Copy C
         this.id = s.id;
         this.name = s.name;
     }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + "]";
+    }
+
 }
