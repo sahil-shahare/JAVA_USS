2f5db74726356a31f46c4c723f2d11ab926b2fcf
sahil-shahare <sahilshahare380@gmail.com>
2026-02-20 13:49:55 +0530

    Collect

--- StreamAPI/Collect/First.java
+++ StreamAPI/Collect/First.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class First {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-                22, 23, 24, 25, 26, 27, 28, 29, 30);
+                22, 23, 24, 25, 26, 27, 28, 29);
 
         List<Integer> evenList = list.stream().filter((i) -> ((i & 1) == 0)).collect(Collectors.toList());
         System.out.println(evenList);
