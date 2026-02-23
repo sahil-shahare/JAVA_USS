11c8497c5f1c06442c0d729d157599605c9e863b
sahil-shahare <sahilshahare380@gmail.com>
2026-02-23 11:57:59 +0530

    Collect

--- String/P1.java
+++ String/P1.java
@@ -1,13 +1,14 @@
 package String;
 
 public class P1 {
-    public static void printReverse(String s1){
-        for(int i=s1.length()-1;i>=0;i--){
+    public static void printReverse(String s1) {
+        for (int i = s1.length() - 1; i >= 0; i--) {
             System.out.print(s1.charAt(i));
         }
     }
+
     public static void main(String[] args) {
-        String s1="I Love Java";
+        String s1 = "I Love Java";
         printReverse(s1);
     }
 }
