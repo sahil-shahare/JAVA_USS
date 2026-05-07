2eeae479432bf78a9521f035fe3cf0964895ebd6
sahil-shahare <sahilshahare380@gmail.com>
2026-05-07 15:31:49 +0530

    Daemon thread

---
+++ MultiThread/Advance/DaemonOne.java
@@ -0,0 +1,22 @@
+package MultiThread.Advance;
+
+public class DaemonOne {
+    public static void main(String[] args) {
+        Thread daemon = new Thread(()->{
+            while(true){
+                System.out.println("Daemon thread running...");
+            }
+        });
+
+        daemon.setDaemon(true);
+
+        Thread user = new Thread(()->{
+            for(int i=1;i<=5;i++){
+                System.out.println("User thread: "+i);
+            }
+        });
+
+        daemon.start();
+        user.start();
+    }
+}
